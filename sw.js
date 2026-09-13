8ff11347440c6234a0dc5920b49d8cdbb8cc2100
Vic <125237471+vicsanity623@users.noreply.github.com>
2026-09-13 00:16:43 -0700

    PlayerInfoModalFeatures

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Bump this version string whenever you deploy an update!
-const CACHE_NAME = 'elden-earth-v800.0.0';
+const CACHE_NAME = 'elden-earth-v800.1.04b';
 
 const ASSETS_TO_CACHE = [
     './',
