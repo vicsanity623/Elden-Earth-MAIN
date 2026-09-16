f32c476729d3b1d70b7cfc9e089a9241376be9ba
Vic <125237471+vicsanity623@users.noreply.github.com>
2026-09-15 21:53:00 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Bump this version string whenever you deploy an update!
-const CACHE_NAME = 'elden-EARTH-v01.02';
+const CACHE_NAME = 'elden-EARTH-v01.03';
 
 const ASSETS_TO_CACHE = [
     './',
