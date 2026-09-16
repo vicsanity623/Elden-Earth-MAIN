34fed4a22e88675be1a29a3663637d0badf39065
VIC <vic@VICs-iMac.local>
2026-09-16 01:35:41 -0700

    Add public landing page for AdSense approval and store listing

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Bump this version string whenever you deploy an update!
-const CACHE_NAME = 'elden-EARTH-v01.07';
+const CACHE_NAME = 'elden-EARTH-v01.23b';
 
 const ASSETS_TO_CACHE = [
     './',
