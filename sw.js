3ecb55d59949867018cfe215a8c1b48edb14bb61
VIC <vic@VICs-iMac.local>
2026-09-15 23:59:17 -0700

    Update manifest for PWABuilder/Play Store

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Bump this version string whenever you deploy an update!
-const CACHE_NAME = 'elden-EARTH-v01.06';
+const CACHE_NAME = 'elden-EARTH-v01.07';
 
 const ASSETS_TO_CACHE = [
     './',
