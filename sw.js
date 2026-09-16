753e75e796d7b83917cba97ee79bd6b34cd5563d
VIC <vic@VICs-iMac.local>
2026-09-15 22:27:45 -0700

    fix: cloud sync gate unblocks purchases, session claim errors no longer brick game

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Bump this version string whenever you deploy an update!
-const CACHE_NAME = 'elden-EARTH-v01.03';
+const CACHE_NAME = 'elden-EARTH-v01.05';
 
 const ASSETS_TO_CACHE = [
     './',
