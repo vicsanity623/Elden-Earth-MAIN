dbf9961aec9d5bc6e7f81ff1b10bb327d7b50bbf
VIC <vic@VICs-iMac.local>
2026-09-17 14:01:23 -0700

    PrivacyPolicy

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Bump this version string whenever you deploy an update!
-const CACHE_NAME = 'elden-EARTH-v18.25';
+const CACHE_NAME = 'elden-EARTH-v19.25';
 
 const ASSETS_TO_CACHE = [
     './',
