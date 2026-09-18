53c092e3d5f764d98f55e73fade9d52d68ab4516
VIC <vic@VICs-iMac.local>
2026-09-17 20:35:07 -0700

    fixbugs3

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Bump this version string whenever you deploy an update!
-const CACHE_NAME = 'elden-EARTH-v19.30';
+const CACHE_NAME = 'elden-EARTH-v19.32';
 
 const ASSETS_TO_CACHE = [
     './',
