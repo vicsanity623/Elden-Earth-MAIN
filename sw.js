5f061b7820c8d88f0b7990aaa92113a3094cddc4
VIC <vic@VICs-iMac.local>
2026-09-17 21:29:13 -0700

    Move economy and territory mutations server-side

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Bump this version string whenever you deploy an update!
-const CACHE_NAME = 'elden-EARTH-v19.32';
+const CACHE_NAME = 'elden-EARTH-v19.33';
 
 const ASSETS_TO_CACHE = [
     './',
